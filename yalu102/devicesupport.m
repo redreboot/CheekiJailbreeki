c5c00a0e781b801dbf1af0849605e48121ef4dcc
Sem Voigtlander <semvoigtlander@Sems-Mac.local>
2017-08-24 18:41:55 +0200

    Making the kvass.

---
+++ yalu102/devicesupport.m
@@ -0,0 +1,9 @@
+//
+//  devicesupport.m
+//  yalu102
+//
+//  Created by Sem Voigtlander on 8/24/17.
+//  Copyright © 2017 kimjongcracks. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
